b42bc04f60428635bfca00ffb303290600112438
2ndyrGroupA <MY_NAME@example.com>
2019-09-28 17:21:13 +0800

    Number 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import WordCounter from './WordCounter';
+
+class App extends Component{
+render(){
+  return(
+    <div>
+      <WordCounter/>
+    </div>
+  )
+    
+  }
+}
+
+
+export default App;
